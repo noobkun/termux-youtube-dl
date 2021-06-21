77d0add60aa98cbee2ab75a6fffdf2f6d5eec81f
noobkun <20661493+noobkun@users.noreply.github.com>
2021-06-21 21:54:36 +0300

    Update share2youtubedl.sh

--- share2youtubedl.sh
+++ share2youtubedl.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
 clear
-
-if [[ "$1" =~ ^.*youtu.*$ ]] || [[ "$1" =~ ^.*youtube.*$ ]]; then
-  echo -e "Downloading video...\\n>URL: ${1}\\n"
-  youtube-dl -F "$1"
-  read -p "Choose your video quality (press enter for: 'best') : " video
-  read -p "Choose your audio quality (press enter for: 'best') : " audio
-  if [[ "$video" = "" ]]; then
-    video="best"
-  fi
-  if [[ "$audio" = "" ]]; then
-    audio="best"
-  fi
-  youtube-dl -f "$video"+"$audio" "$1"
-elif [[ "$1" =~ ^.*nourlselected.*$ ]]; then
-  echo "There was an error"
-else youtube-dl -f best "$1"
+#script to download Youtube videos to mp3,m4a,mp4
+ 
+# Usage
+# install Termux and setup the tool 
+#for simplicity converting to mp3 is done by pressing enter after sharing video to Termux
+clear
+ 
+echo "For audio m4a audio only press 1"
+echo "For video 360p press 2"
+echo "For video 480p press 3"
+echo "For video 720p press 4"
+echo "For video 1080p press 5"
+ 
+command='--external-downloader aria2c --external-downloader-args \"-j 16 -s 16 -x 16 -k 5M\" --no-mtime -o /data/data/com.termux/files/home/storage/shared/Movies/%(title)s.%(ext)s -f'
+read option
+ 
+if [ "$option" -eq "1" ]
+then
+    clear
+    echo "--external-downloader aria2c --external-downloader-args \"-j 16 -s 16 -x 16 -k 5M\" --no-mtime -o /data/data/com.termux/files/home/storage/shared/Music/%(title)s.%(ext)s -f bestaudio[ext=m4a] --audio-format mp3 --prefer-ffmpeg" > ~/.config/youtube-dl/config
+    youtube-dl $1
+elif [ "$option" -eq "2" ]
+then
+    clear
+    echo "$command \"best[height<=360]\"" > ~/.config/youtube-dl/config
+    youtube-dl $1
+ 
+elif [ "$option" -eq "3" ]
+then
+    clear
+    echo "$command \"best[height<=480]\"" > ~/.config/youtube-dl/config
+    youtube-dl $1
+ 
+elif [ "$option" -eq "4" ]
+then
+    clear
+    echo "$command \"best[height<=720]\"" > ~/.config/youtube-dl/config
+    youtube-dl $1
+ 
+elif [ "$option" -eq "5" ]
+then
+    clear
+    echo "$command \"best[height<=1080]\"" > ~/.config/youtube-dl/config
+    youtube-dl $1
 fi
-
-read -p "Press enter to continue"
